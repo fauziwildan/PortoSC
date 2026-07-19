520dc61fe54c90ac2e9e97d5af8669f55b04f445
fauziwildan <fauziwildan.widyatama@gmail.com>
2026-07-19 23:35:42 +0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,27 @@ import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-        }),
-        inertia(),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
-        }),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
+export default defineConfig(({ command }) => {
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.tsx'],
+                refresh: true,
+                fonts: [
+                    bunny('Instrument Sans', {
+                        weights: [400],
+                    }),
+                ],
+            }),
+            inertia(),
+            react({
+                babel: {
+                    plugins: ['babel-plugin-react-compiler'],
+                },
+            }),
+            tailwindcss(),
+            // Only execute wayfinder locally, remove it from production Vercel builds
+            command === 'serve' ? wayfinder({ formVariants: true }) : null,
+        ].filter(Boolean), // Removes the null value from the plugin array in production
+    };
 });
