eb00b26dffa1a1fde45e544e681f5f65d8d0a596
fauziwildan <fauziwildan.widyatama@gmail.com>
2026-07-20 09:52:08 +0700

    migrate

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Database successfully migrated!';
+});
+
+
